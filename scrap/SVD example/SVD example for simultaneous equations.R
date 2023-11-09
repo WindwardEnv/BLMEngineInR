7d024e03f69a4f77d455dc573969e8e868407052
RobertSantore <147429220+RobertSantore@users.noreply.github.com>
2023-11-08 19:22:45 -0500

    Create SVD example for simultaneous equations.R

---
+++ scrap/SVD example/SVD example for simultaneous equations.R
@@ -0,0 +1,42 @@
+# Define a matrix and an x vector
+a = matrix(nrow=4, ncol=4, c(1, 3, 5, 3, 5, 1, 33, 4, 5, 1, 23, 4, 5, 6, 4, 2))
+x=c(5, 3, 7, 8)
+
+# create y 
+y=a %*% x
+
+# now, we have a, x, and y such that a*x = y
+
+
+# ----------------------------------------------------
+# Here's the fun bit - 
+# pretend we don't know x, find it from a and y
+# ----------------------------------------------------
+
+# store the SVD of a
+asvd = svd(a)
+
+# store all the decomposition results
+d = diag(asvd$d)
+dinv = diag(1/asvd$d)
+U = asvd$u
+VT = asvd$v
+
+UT = t(U)
+V = t(VT)
+
+# verify that we can get matrix a back 
+acopy = asvd$u %*% diag(asvd$d) %*% t(asvd$v)
+acopy = U %*% d %*% V
+
+# find the matrix inverse of a by SVD
+ainv1 = VT %*% dinv %*% UT
+
+# find the matrix inverse of a by solve
+ainv2 = solve(a)
+
+# which method is faster?
+
+# solve for x
+ainv1 %*% y
+
