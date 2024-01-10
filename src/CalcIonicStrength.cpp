ce972d023ed73fddb79adf972012590c43ea74e3
Kelly Croteau <kellyc@windwardenv.com>
2024-01-10 10:42:49 -0500

    Adding Davies

--- src/CalcIonicStrength.cpp
+++ src/CalcIonicStrength.cpp
@@ -47,19 +47,19 @@ Rcpp::NumericVector ExtDebyeHuckel(double IonicStrength,
   Rcpp::NumericVector ActivityCoefDebye(MaxCharge + 1);
 
   /* variables */
-  int ZZ;
+  int AbsCharge;
   double A, B, C, D;
-  int E, F;
+  int F;
 
   A = 0.27 + (0.0008 * SysTempKelvin);
   B = 0.33;
   C = -A * sqrt(IonicStrength);
   D = B * sqrt(IonicStrength);
 
-  for (ZZ = 0; ZZ <= MaxCharge; ZZ++) {
-    E = abs(ZZ);
-    F = ZZ * ZZ;
-    ActivityCoefDebye(ZZ) = pow(10, F * C / (1 + (3 * E * D)));
+  for (AbsCharge = 0; AbsCharge <= MaxCharge; AbsCharge++) {
+    //E = abs(AbsCharge);//redundant
+    F = pow(AbsCharge, 2);
+    ActivityCoefDebye(AbsCharge) = pow(10, F * C / (1 + (3 * AbsCharge * D)));
   }
   
 /* This is what's done in the PB code...Is this even necessary?? Doesn't the 
@@ -76,6 +76,38 @@ Rcpp::NumericVector ExtDebyeHuckel(double IonicStrength,
   return ActivityCoefDebye;
 }
 
+Rcpp::NumericVector Davies(double IonicStrength, int MaxCharge) {
+  
+  /* outputs */
+  Rcpp::NumericVector ActivityCoefDavies(MaxCharge + 1);
+  
+  /* variables */
+  int AbsCharge;
+  double SqrtIonicStrength = sqrt(IonicStrength);
+  double DaviesConst;
+  double Tmp;
+
+  // This part is the same for every charge...
+  DaviesConst = -0.5 * ((SqrtIonicStrength / (1 + SqrtIonicStrength)) 
+                        - 0.2 * IonicStrength);
+
+  for (AbsCharge = 0; AbsCharge <= MaxCharge; AbsCharge++) {
+    if (AbsCharge == 0) { 
+      ActivityCoefDavies(AbsCharge) = 1;
+    } else {
+      Tmp = pow(AbsCharge, 2) * DaviesConst;
+      if (Tmp > 3.0) {
+        ActivityCoefDavies(AbsCharge) = 1;
+      } else {
+        ActivityCoefDavies(AbsCharge) = pow(10, Tmp);
+      }
+    }
+  }
+  
+  return ActivityCoefDavies;
+
+}
+
 Rcpp::NumericVector CalcActivityCoef(unsigned int NSpec,
                                      Rcpp::CharacterVector SpecActCorr,
                                      Rcpp::IntegerVector SpecCharge,
@@ -89,15 +121,17 @@ Rcpp::NumericVector CalcActivityCoef(unsigned int NSpec,
   unsigned int iSpec;
   int MaxCharge = Rcpp::max(Rcpp::abs(SpecCharge));
   Rcpp::NumericVector ActivityCoefDebye;
+  Rcpp::NumericVector ActivityCoefDavies;
   
   ActivityCoefDebye = ExtDebyeHuckel(IonicStrength, MaxCharge, SysTempKelvin);
   //Rcpp::Rcout << "ActivityCoefDebye = " << ActivityCoefDebye << std::endl;
-
+  ActivityCoefDavies = Davies(IonicStrength, MaxCharge);
+  
   for (iSpec = 0; iSpec < NSpec; iSpec++) {
     if (SpecActCorr(iSpec) == "Debye") {
       SpecActivityCoef(iSpec) = ActivityCoefDebye(abs(SpecCharge(iSpec)));
-    //} else if (SpecActCorr(iSpec) == "Davies") {
-    //  SpecActivityCoef(iSpec) = Davies(IonicStrength, SpecCharge(iSpec));
+    } else if (SpecActCorr(iSpec) == "Davies") {
+      SpecActivityCoef(iSpec) = ActivityCoefDavies(abs(SpecCharge(iSpec)));
     } else {//if (SpecActCorr(iSpec) == "None") {
       SpecActivityCoef(iSpec) = 1.0;
     }
