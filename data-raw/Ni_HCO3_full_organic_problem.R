99502b3fb135f56951e9ba6fdafa19a07fa519d4
KellyCroteau <kellyc@windwardenv.com>
2025-08-07 13:31:46 -0400

    Adding copyright header and check

--- data-raw/Ni_HCO3_full_organic_problem.R
+++ data-raw/Ni_HCO3_full_organic_problem.R
@@ -1,3 +1,17 @@
+# Copyright 2024 Windward Environmental LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # The Ni problem for organisms sensitive to bicarbonate
 Ni.w.pfile = normalizePath("C:/Users/kellyc/documents/BLM/parameter_files/fresh/Ni/Ni_all_studies_calibration_toxic_carbonates_2024-04-10.dat")
 Ni.r.pfile = file.path("inst", "extdata", "ParameterFiles", "Ni_HCO3_full_organic.dat4")
